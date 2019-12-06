577b5bacc1c553442167d2b12ece719f056443c3
mphelps1978 <phelps.michael@live.com>
2019-12-06 10:43:08 -0600

    Header MVP Complete

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,38 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+// first, let's define our entryPoint for the component. In this case, it's the header, so we need to select the element that has the header in it. (.header-container)
+
+const headerEntry = document.querySelector('.header-container');
+
+function header() {
+	// let's create variables for each of the elements
+
+	const header = document.createElement('div'),
+		date = document.createElement('span'),
+		headerTitle = document.createElement('h1'),
+		temp = document.createElement('span');
+
+	// now style them
+
+	header.classList.add('header');
+	date.classList.add('date');
+	temp.classList.add('temp');
+
+	//this needs some text, so let's add it
+	date.textContent = 'MARCH 28, 2019';
+	headerTitle.textContent = 'Lambda Times';
+	temp.textContent = '98°';
+
+	// now we set up the parent/child relationships.
+
+	header.appendChild(date);
+	header.appendChild(headerTitle);
+	header.appendChild(temp);
+	console.log(header);
+	return header;
+}
+
+// now lets get it onto the page
+
+headerEntry.appendChild(header());
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,12 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios
+	.get('https://lambda-times-backend.herokuapp.com/topics')
+	.then(response => {
+		console.log(response);
+	})
+	.catch(error => {
+		console.log(`Something's Wrong: ${error}`);
+	});
